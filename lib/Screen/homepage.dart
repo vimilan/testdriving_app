fda31bdd64f91393a7a067b9db51a7f17c01c55b
Vikas <coolram1114@gmail.com>
2020-02-07 17:03:03 +0530

    s

--- lib/Screen/homepage.dart
+++ lib/Screen/homepage.dart
@@ -11,44 +11,47 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-         length: 6,
+         length: 3,
       child: Scaffold(
         appBar: AppBar(
           title: Text('MyPage'),
          bottom: TabBar(
             tabs: <Widget>[
               Tab(
-                child: Text('Event'),
+                child: Text('About'),
               ),
               Tab(
-                child: Text('Event' ),
+                child: Text('Contact' ),
               ),
               Tab(
-                child: Text('Event'),
+                child: Text('Logout'),
               ),
-              Tab(
-                child: Text('Event'),
-              ),
-              Tab(
-                child: Text('Event'),
-              ),
-              Tab(
-                child: Icon(Icons.arrow_forward_ios,),
-              ),
+
             ],
           ),
         ),
          drawer: Drawer(
+           child: ListView(
+             children: <Widget>[
+               DrawerHeader(
+                 child: Text('Username'),
+               ),
+               ListTile(
+                 title: Text(
+                   'Electronics'
+                 ),
+                 trailing: Icon(Icons.account_box),
+                 leading: Icon(Icons.arrow_forward),
+               )
+             ],
+           ),
          ),
          body: TabBarView(
            children: <Widget>[
              Tablet,
              Icon(Icons.arrow_forward_ios),
              Icon(Icons.arrow_forward_ios),
-             Tablet,
-             Icon(Icons.arrow_forward_ios),
-             Icon(Icons.arrow_forward_ios)
-           ],
+    ],
          ),
       ),
     );
